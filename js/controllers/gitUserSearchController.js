f367450e078491fb7183c957462ce95bcb21313d
armi1189 <armi1189@gmail.com>
2015-04-29 14:20:35 +0100

    All done

--- js/controllers/gitUserSearchController.js
+++ js/controllers/gitUserSearchController.js
@@ -1,3 +1,10 @@
-githubUserSearch.controller('GitUserSearchController', [function(){
-  
+githubUserSearch.controller('GitUserSearchController', ['$resource', function($resource){
+  var self = this;
+  var searchResource = $resource('https://api.github.com/search/users?access_token=7458b50664a5209ab61c80fc7b3b86c494f3226c');
+
+  self.doSearch = function(){
+    self.searchResult = searchResource.get(
+      { q: self.searchTerm }
+    );
+  };
 }]);
